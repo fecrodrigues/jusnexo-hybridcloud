9226422ec06de3614bccb7c5ee2b2b2731573d97
Ubuntu <ubuntu@ip-172-31-22-34.ec2.internal>
2021-05-25 21:41:47 +0000

    Initial version of jusnexo generated by JHipster-7.0.1

---
+++ src/main/java/br/com/jusnexo/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package br.com.jusnexo.repository;
+
+import br.com.jusnexo.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
